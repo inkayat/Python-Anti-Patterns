f5e1c8bbf6b967d0bf77b4af9015b13d580232d8
tolga inkaya <redlof@tolgas-MacBook-Pro.local>
2021-12-03 00:34:04 +0300

    else clause break statement

--- correctness.py
+++ correctness.py
@@ -25,5 +25,25 @@
         except Exception as e:
             print("Exception")
 
+    3.Else clause on loop without a break statement
+       
+        [Anti-patttern]
+        def contains_magic_number(list, magic_number):
+            for i in list:
+                if i == magic__number:
+                    print("This list contains the magic number")
+            else:
+                print("This list does NOT contain the magic number")
+
+        [Best practices]
+        def contains_magic_number(list, magic_number):
+            for i in list:
+                if i == magic_number:
+                    print("This list contains the magic number.")
+                    #added break statement
+                    break
+            else:
+                print("This list does NOT contain the magic number.")
+
 
 """
